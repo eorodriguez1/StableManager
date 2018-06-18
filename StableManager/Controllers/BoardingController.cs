f18ec415a9ed4750c55d9e9224523a618520d161
edwin rodriguez <eorodrig@live.com>
2018-06-18 11:19:49 -0600

    Updated application. Waiting on JS/angular update to complete project

--- StableManager/Controllers/BoardingController.cs
+++ StableManager/Controllers/BoardingController.cs
@@ -22,14 +22,22 @@ namespace StableManager.Controllers
             _context = context;
         }
 
-        // GET: Boarding
+        /// <summary>
+        /// Returns a list of all boardings
+        /// </summary>
+        /// <returns></returns>
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Boardings.Include(b => b.Animal).Include(b => b.BoardingType).Include(b => b.BillToUser);
             return View(await applicationDbContext.ToListAsync());
         }
 
-        // GET: Boarding/Details/5
+
+        /// <summary>
+        /// Returns a specific boarding detail
+        /// </summary>
+        /// <param name="id">id of boarding</param>
+        /// <returns></returns>
         public async Task<IActionResult> Details(string id)
         {
             if (id == null)
@@ -37,11 +45,13 @@ namespace StableManager.Controllers
                 return NotFound();
             }
 
+            //attach required properties to each boarding
             var boarding = await _context.Boardings
                 .Include(b => b.Animal)
                 .Include(b => b.BoardingType)
                 .Include(b => b.BillToUser)
                 .SingleOrDefaultAsync(m => m.BoardingID == id);
+
             if (boarding == null)
             {
                 return NotFound();
@@ -54,7 +64,7 @@ namespace StableManager.Controllers
             return View(boarding);
         }
 
-        // GET: Boarding/Create
+        //create a boarding
         public IActionResult Create()
         {
             ViewData["AnimalID"] = new SelectList(_context.Animals, "AnimalID", "AnimalName");
@@ -64,14 +74,14 @@ namespace StableManager.Controllers
         }
 
         // POST: Boarding/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //Saving a boarding
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("BoardingID,AnimalID,BillToUserID,BoardingTypeID,StartedBoard,EndedBoard")] Boarding boarding)
         {
             if (ModelState.IsValid)
             {
+                //add the require modified when/by stamps
                 var CurrentUser = await _context.Users.FirstOrDefaultAsync(u => u.UserName == User.Identity.Name);
                 boarding.ModifierUserID = CurrentUser.FirstName + " " + CurrentUser.LastName;
                 boarding.ModifiedOn = DateTime.Now;
@@ -85,7 +95,11 @@ namespace StableManager.Controllers
             return View(boarding);
         }
 
-        // GET: Boarding/Edit/5
+        /// <summary>
+        /// get boarding for edit
+        /// </summary>
+        /// <param name="id">id of boarding to modify</param>
+        /// <returns></returns>
         public async Task<IActionResult> Edit(string id)
         {
             if (id == null)
@@ -105,8 +119,12 @@ namespace StableManager.Controllers
         }
 
         // POST: Boarding/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Saving an editted boarding
+        /// </summary>
+        /// <param name="id">boarding id</param>
+        /// <param name="boarding">boarding object to save</param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(string id, [Bind("BoardingID,AnimalID,BillToUserID,BoardingTypeID,StartedBoard,EndedBoard")] Boarding boarding)
@@ -120,6 +138,7 @@ namespace StableManager.Controllers
             {
                 try
                 {
+                    //add modified by/when stamps
                     var CurrentUser = await _context.Users.FirstOrDefaultAsync(u => u.UserName == User.Identity.Name);
                     boarding.ModifierUserID = CurrentUser.FirstName + " " + CurrentUser.LastName;
                     boarding.ModifiedOn = DateTime.Now;
